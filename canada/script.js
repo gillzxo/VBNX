c87e0936be9f141810512bef187c407973c0406c
Jagdeep gill [jatt life thuglife] <64634071+gillzxo@users.noreply.github.com>
2022-11-26 14:00:10 +0530

    Update script.js

--- canada/script.js
+++ canada/script.js
@@ -17,6 +17,23 @@ $(document).ready(function(){
     });
 });
 
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
 /* ===== SCROLL REVEAL ANIMATION ===== */
 const srtop = ScrollReveal({
     origin: 'top',
