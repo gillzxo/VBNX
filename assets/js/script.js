c853e6f21791b740cbff18f63f05e2504eaabacb
Jagdeep gill [jatt life thuglife] <64634071+gillzxo@users.noreply.github.com>
2022-12-14 15:57:45 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -97,7 +97,7 @@ function showSkills(skills) {
         <div class="bar">
               <div class="info">
                 <img src=${skill.icon} alt="skill" />
-              
+                <span>${skill.name}</span>
               </div>
             </div>`
     });
